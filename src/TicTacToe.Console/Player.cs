1a6164e4ae6220e05ca980c13a5e019246dc8396
m Warner <mwarner1@jeffersonstate.edu>
2023-02-23 21:17:32 -0600

    Adds score system functions

--- src/TicTacToe.Console/Player.cs
+++ src/TicTacToe.Console/Player.cs
@@ -3,9 +3,16 @@
     public class Player
     {
         char symbol;
+        int wins;
+        int losses;
+        int ties;
         public Player()
         {
             symbol = ' ';
+            wins = 0;
+            losses = 0;
+            ties = 0;
+            
         }
         public void setPlayerSymbol(char playerSymbol)
         {
@@ -16,6 +23,34 @@
             return symbol;
         }
 
+        public int getPlayerWins()
+        {
+            return wins;
+        }
+        public int getPlayerLosses()
+        {
+            return losses;
+        }
+        public int getTies()
+        {
+            return ties;
+        }
+
+        public void addPlayerWin()
+        {
+            wins++;
+        }
+        public void addPlayerLoss()
+        {
+            losses++;
+        }
+
+        public void addTie()
+        {
+            ties++;
+        }
+      
+       
     }
 }
 
