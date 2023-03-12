93e870aecda969243d796c7c43f35a086ee8d544
m Warner <mwarner1@jeffersonstate.edu>
2023-03-11 23:43:49 -0600

    added total game time spent to Game

--- src/TicTacToe.Console/Game.cs
+++ src/TicTacToe.Console/Game.cs
@@ -173,7 +173,7 @@ namespace TicTacToe
             WriteLine("\t{0}\t\t{1}", pc.getPlayerWins(), pc.getPlayerLosses());
             WriteLine("Times Tied = {0}", user.getTies());
             WriteLine("-----------------------------------");
-            WriteLine("\t\t Time spent in game: {0:00:00}  \t Time spent on average {1:00:00} ",timerStats.getGameTime(), timerStats.getAvgGameTime());
+            WriteLine("\t\t Time spent in game: {0:00:00}  \t Time spent on average: {1:00:00}  \t Total time spent across all games: {2:00:00} ",timerStats.getGameTime(), timerStats.getAvgGameTime(),timerStats.getTotalGameTime());
 
         }
 
