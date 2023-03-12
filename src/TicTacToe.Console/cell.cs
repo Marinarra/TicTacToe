4fea48e9d1e35be6091eb6d44d401c1be0194ae0
m Warner <mwarner1@jeffersonstate.edu>
2023-03-11 23:05:55 -0600

    created timer class

--- src/TicTacToe.Console/cell.cs
+++ src/TicTacToe.Console/cell.cs
@@ -1,9 +1,38 @@
-﻿namespace TicTacToe
+﻿using System.Diagnostics;
+namespace TicTacToe
 {
-    public enum Cell
-    {
-        cellEmpty = ' ',  //0
-        cellFilledO = 'O',  //1
-        cellFilledX = 'X'  //2
-    }
+      public class gameTimeStats
+    
+      {
+
+        Stopwatch timer = new Stopwatch();
+        public double gameTime = 0.00, avgGameTime = 0.00;
+
+        public void startTimer()
+        {
+            timer.Start();
+        }
+        public void stopTime()
+        {
+            setAvgGameTime();
+            setGameTime();
+            timer.Reset();
+        }
+        private void setGameTime()
+        {
+            gameTime = timer.ElapsedMilliseconds;
+        }
+        public void setAvgGameTime()
+        {
+            avgGameTime = avgGameTime + timer.ElapsedMilliseconds;
+        }
+        public double getGameTime()
+        {
+            return gameTime;
+        }
+        public double getAvgGameTime()
+        {
+            return avgGameTime;
+        }
+     }
 }
