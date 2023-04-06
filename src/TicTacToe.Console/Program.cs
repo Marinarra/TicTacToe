a4f9460ab4a4a2acb3465afdcf2d713b87b683a1
m Warner <mwarner1@jeffersonstate.edu>
2023-04-05 22:35:07 -0500

    text for menu added

--- src/TicTacToe.Console/Program.cs
+++ src/TicTacToe.Console/Program.cs
@@ -1,12 +1,39 @@
-﻿namespace TicTacToe
+﻿using static System.Console;
+namespace TicTacToe
 {
     public class Program
     {
         static void Main(string[] args)
         {
 
-            Game.startGame();    
+            printMenuText();
+            // Game.startGame();
+
+        }
+         private static void printMenuText()
+            {
+            string[] options = {"New Game","Game Stats","Exit Game"};
+            WriteLine(" \t######                         #     #\n" +
+                      "\t#     #   ##   #    # ######    ##   ## ###### #    # #    # \n" +
+                      "\t#        #  #  ##  ## #         # # # # #      ##   # #    # \n" +
+                      "\t#  #### #    # # ## # #####     #  #  # #####  # #  # #    # \n" +
+                      "\t#     # ###### #    # #         #     # #      #  # # #    # \n" +
+                      "\t#     # #    # #    # #         #     # #      #   ## #    # \n" +
+                      "\t#####  #    # #    # ######    #     # ###### #    #  ####   \n");
+                                                              
+
+
+                WriteLine("**************************************** \n");
+                for (int i = 0; i < 3; i++)
+                {
+                    WriteLine("\t  -||============-+-> \n" +
+                              "\t [{0}] {1} {0} -+-> Press {0} (=^._.^=)∫ \n" +
+                              "\t  -||============-+-> \n\n", i + 1, options[i]);
+                }
+            }
+        
+
+
         }
 
     }
-}
