f1fc06acfc8f8e25c744a170edafe41e6e76f4f6
m Warner <mwarner1@jeffersonstate.edu>
2023-04-22 23:58:41 -0500

    Converted to Struct

--- src/TicTacToe.Console/Player.cs
+++ src/TicTacToe.Console/Player.cs
@@ -1,56 +1,14 @@
 ﻿namespace TicTacToe
 {
-    public class Player
-    {
-        char symbol;
-        int wins;
-        int losses;
-        int ties;
-        public Player()
+   
+        public struct Player
         {
-            symbol = ' ';
-            wins = 0;
-            losses = 0;
-            ties = 0;
+           public char symbol;
+           public int wins;
+           public int losses;
+           public int ties;
             
         }
-        public void setPlayerSymbol(char playerSymbol)
-        {
-            symbol = playerSymbol;
-        }
-        public char getSymbol()
-        {
-            return symbol;
-        }
-
-        public int getPlayerWins()
-        {
-            return wins;
-        }
-        public int getPlayerLosses()
-        {
-            return losses;
-        }
-        public int getTies()
-        {
-            return ties;
-        }
-
-        public void addPlayerWin()
-        {
-            wins++;
-        }
-        public void addPlayerLoss()
-        {
-            losses++;
-        }
-
-        public void addTie()
-        {
-            ties++;
-        }
-      
-       
-    }
+  
 }
 
