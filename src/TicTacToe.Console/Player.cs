cb4cf62cc6026950599b7744d824f75d58dcc046
m Warner <mwarner1@jeffersonstate.edu>
2023-04-23 01:10:40 -0500

    Added Constructor

--- src/TicTacToe.Console/Player.cs
+++ src/TicTacToe.Console/Player.cs
@@ -7,8 +7,15 @@
            public int wins;
            public int losses;
            public int ties;
-            
-        }
+        
+            public Player(char _symbol, int _wins, int _losses, int _ties)
+            {
+                symbol = _symbol;
+                wins = _wins;
+                losses = _losses;
+                ties = _ties;
+            }
+    }
   
 }
 
