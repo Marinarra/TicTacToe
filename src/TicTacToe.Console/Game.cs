8a4c4172310676bbeab08369c1dc12abdd1951ce
m Warner <mwarner1@jeffersonstate.edu>
2023-04-30 13:14:08 -0500

    Changed if statements into if-else

--- src/TicTacToe.Console/Game.cs
+++ src/TicTacToe.Console/Game.cs
@@ -215,10 +215,9 @@ namespace TicTacToe
                     flagTurn = user.symbol;
                     updateScreen();
                 }
-
                 isGameWon = (board.checkWinCondition(pc.symbol) || board.checkWinCondition(user.symbol) || board.checkTieCondition()); // checks if pc win OR user win OR tie
                 //  Checks if the flagTurn is for the users symbol and if the game hasn't been won, if so the scren is updated, the user is given a coordinate selection screen and then the flag is given to the system 
-                if (flagTurn == user.symbol && isGameWon == false)  // user turn
+                else if (flagTurn == user.symbol && isGameWon == false)  // user turn
                 {
                     gameMenu();
                     flagTurn = pc.symbol;
